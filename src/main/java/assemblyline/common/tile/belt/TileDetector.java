fa4d1d5a857be70b1a35705f675f582b009a23a2
skip999 <skiprocks999@gmail.com>
2025-01-17 16:05:02 -0600

    clean up code

--- src/main/java/assemblyline/common/tile/belt/TileDetector.java
+++ src/main/java/assemblyline/common/tile/belt/TileDetector.java
@@ -6,6 +6,7 @@ import assemblyline.common.tile.belt.utils.GenericTileConveyorBelt;
 import assemblyline.registers.AssemblyLineTiles;
 import electrodynamics.prefab.tile.GenericTile;
 import electrodynamics.prefab.tile.components.type.ComponentTickable;
+import electrodynamics.prefab.utilities.object.CachedTileOutput;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
@@ -22,7 +23,10 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 
 public class TileDetector extends GenericTile {
+
 	public boolean isPowered = false;
+
+	private CachedTileOutput beltCache;
 
 	public TileDetector(BlockPos worldPosition, BlockState blockState) {
 		super(AssemblyLineTiles.TILE_DETECTOR.get(), worldPosition, blockState);
@@ -30,25 +34,39 @@ public class TileDetector extends GenericTile {
 	}
 
 	public void tickServer(ComponentTickable component) {
-		if (component.getTicks() % 4 == 0) {
-			return;
-		}
-		BlockPos relative = worldPosition.relative(getFacing());
 
-		List<ItemEntity> entities = level.getEntities(EntityType.ITEM, new AABB(relative), entity -> entity != null && !entity.getItem().isEmpty());
-		if (!entities.isEmpty()) {
-			if (!isPowered) {
+		if(beltCache == null) {
+			beltCache = new CachedTileOutput(getLevel(), getBlockPos().relative(getFacing()));
+		}
+
+		if(component.getTicks() % 10 == 0 && !(beltCache.getSafe() instanceof GenericTileConveyorBelt)) {
+			beltCache.update(getBlockPos().relative(getFacing()));
+		}
+
+		if(beltCache.getSafe() instanceof GenericTileConveyorBelt belt) {
+
+			if(belt.getItemOnBelt().isEmpty() && isPowered) {
+				isPowered = false;
+				level.updateNeighborsAt(worldPosition, getBlockState().getBlock());
+			} if(!belt.getItemOnBelt().isEmpty() && !isPowered) {
 				isPowered = true;
 				level.updateNeighborsAt(worldPosition, getBlockState().getBlock());
 			}
-		} else if (level.getBlockEntity(relative) instanceof GenericTileConveyorBelt belt && !belt.getItemOnBelt().isEmpty()) {
-			if(!isPowered) {
-				isPowered = true;
+
+		} else if (component.getTicks() % 4 == 0) {
+
+			BlockPos relative = worldPosition.relative(getFacing());
+
+			List<ItemEntity> entities = level.getEntities(EntityType.ITEM, new AABB(relative), entity -> entity != null && !entity.getItem().isEmpty());
+			if (!entities.isEmpty()) {
+				if (!isPowered) {
+					isPowered = true;
+					level.updateNeighborsAt(worldPosition, getBlockState().getBlock());
+				}
+			}  else if (isPowered) {
+				isPowered = false;
 				level.updateNeighborsAt(worldPosition, getBlockState().getBlock());
 			}
-		} else if (isPowered) {
-			isPowered = false;
-			level.updateNeighborsAt(worldPosition, getBlockState().getBlock());
 		}
 	}
 
